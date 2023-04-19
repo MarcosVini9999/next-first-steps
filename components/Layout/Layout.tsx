7776e681f6acdadcddd16d34a3abeddc83a30dcb
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2023-04-18 21:06:00 -0300

    feat: link preview

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -12,6 +12,19 @@ export function Layout({ children }: LayoutProps) {
       <Head>
         <title>Wheater app</title>
         <link rel="icon" type="image/x-icon" href="/sky-sun-logo.ico"></link>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Weather Location Next" />
+        <meta property="og:site_name" content="Weather Location Next" />
+        <meta
+          property="og:description"
+          content="A website that uses a public weather api to look up any location the user wants"
+        />
+        <meta property="og:url" content="weather-location-next.vercel.app/" />
+        <meta property="og:image" content="/sky-sun-logo.svg" />
+        <meta property="og:image:secure_url" content="/sky-sun-logo.svg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="675" />
+        <meta property="og:image:type" content="image/svg" />
       </Head>
       <div>
         <Header />
